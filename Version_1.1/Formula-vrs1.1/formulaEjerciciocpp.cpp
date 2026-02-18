004b296acde7456a80559979caeacf2665d295e9
Abner Muñoz <abnerxmaa@gmail.com>
2026-02-17 21:59:58 -0600

    Version 1.1

--- Version_1.1/Formula-vrs1.1/formulaEjerciciocpp.cpp
+++ Version_1.1/Formula-vrs1.1/formulaEjerciciocpp.cpp
@@ -1,3 +1,7 @@
+// Este codigo ha sido generado por el modulo psexport 20230904-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
 #include<iostream>
 using namespace std;
 
